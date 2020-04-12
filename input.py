738e59b051ad4cf10db65d584775063088718e10
tejaa487 <63533249+tejaa487@users.noreply.github.com>
2020-04-12 20:02:41 +0530

    seeking input from the user

---
+++ input.py
@@ -0,0 +1,10 @@
+a=float(input("enter 1st num"))
+b=float(input("enter 2nd number:"))
+c=float(input("enter 3rd num:"))
+s=(a+b+c)/2
+print(s)
+area=(s*(s-a)*s*(s-b)*s*(s-c))**0.5
+
+print(area)
+
+
