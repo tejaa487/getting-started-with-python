3cbcb081343cf9e471a76e0e07210dafa193199c
tejaa487 <63533249+tejaa487@users.noreply.github.com>
2020-04-12 20:08:54 +0530

    finding roots of quadratic equations

---
+++ roots of quadeqn.py
@@ -0,0 +1,14 @@
+import cmath
+a=float(input("enter any num:"))
+b=float(input("enter any num:"))
+c=float(input("enter any num:"))
+d=b**2-4*a*c
+e=-b+cmath.sqrt(d)
+f=e/2*a
+g=-b-cmath.sqrt(d)
+h=g/2*a
+print(f,h)
+        
+
+
+        
